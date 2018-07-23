cf43152f6becbf213445907958d04242147dafa5
stefano cafarella <stefano.cafarella@matchesfashion.com>
2018-07-23 18:12:17 +0100

    up

--- src/main/java/com/nutmeg/holdingcalculatator/HoldingCalculatorImpl.java
+++ src/main/java/com/nutmeg/holdingcalculatator/HoldingCalculatorImpl.java
@@ -1,16 +1,32 @@
 package com.nutmeg.holdingcalculatator;
 
+import com.nutmeg.helpers.TransactionsParser;
 import com.nutmeg.transactions.Holding;
+import com.nutmeg.transactions.Transaction;
 
 import java.io.File;
+import java.io.IOException;
 import java.time.LocalDate;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class HoldingCalculatorImpl implements HoldingCalculator {
+    private TransactionService transactionService;
+
+    public HoldingCalculatorImpl() {
+        this.transactionService = new TransactionServiceImpl();
+    }
+
     @Override
     public Map<String, List<Holding>> calculateHoldings(File transactionFile, LocalDate date) {
+        Map<String,List<Holding>> holdings = new HashMap<>();
+        try {
+            List<Transaction> transactions = TransactionsParser.parseTransactions(transactionFile);
 
-    return null;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return holdings;
     }
 }
--- src/main/java/com/nutmeg/holdingcalculatator/TransactionServiceImpl.java
+++ src/main/java/com/nutmeg/holdingcalculatator/TransactionServiceImpl.java
@@ -5,7 +5,9 @@ import com.nutmeg.transactions.ProcessedTransaction;
 import com.nutmeg.transactions.Transaction;
 import com.nutmeg.transactions.TxnType;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 public class TransactionServiceImpl implements TransactionService {
@@ -13,22 +15,21 @@ public class TransactionServiceImpl implements TransactionService {
     private final static String CASH_ASSET = "CASH";
     @Override
     public List<Holding> processTransaction(final Transaction transaction, final LocalDate localDate, final List<Holding> currentHolding) {
-/*        Holding assetHolding = currentHolding.stream()
+        final Holding assetHolding = currentHolding.stream()
                 .filter(holding -> holding.getAsset().equals(transaction.getAsset()))
                 .findFirst()
                 .get();
-        Holding cashHolding = currentHolding.stream()
+        final Holding cashHolding = currentHolding.stream()
                 .filter(holding -> holding.getAsset().equals(CASH_ASSET))
                 .findFirst()
                 .get();
 
         if(transaction.getTxnType().equals(TxnType.BOT)){
-
+            assetHolding.getHolding();
         }
-
+        final List<Holding> updatedHolding = new ArrayList<>();
         currentHolding.forEach(holding -> {
-
-        });*/
+        });
         return null;
 
 
